0fcc50e5edbc797d163081dac0e7d7ba35aaa578
Michael Osei-Bobie <GH031858@fbnbankghana.com>
2022-01-31 21:14:55 +0000

    commiting codebase initially

---
+++ src/main/java/com/codingchallege/crewmeister/CrewmeisterApplication.java
@@ -0,0 +1,13 @@
+package com.codingchallege.crewmeister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewmeisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewmeisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingchallege/crewmeister/CrewmeisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallege.crewmeister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewmeisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
